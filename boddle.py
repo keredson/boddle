d81ee649c22bfd6f63424cc1a1dc4c4fbf1b2676
Derek Anderson <derek@hvst.com>
2016-12-21 11:09:14 -0600

    init

---
+++ boddle.py
@@ -0,0 +1,58 @@
+# GNU LESSER GENERAL PUBLIC LICENSE
+
+import bottle, io
+import json as lib_json
+
+try:
+  from urlparse import urlparse
+  from urllib import urlencode
+except ImportError:
+  from urllib.parse import urlparse, urlencode
+
+class boddle(object):
+
+  def __init__(self, params=None, path=None, method=None, headers=None, json=None, url=None, **extras):
+
+    environ = {}
+    
+    if params is not None:
+      self._set_payload(environ, urlencode(params).encode('utf8'))
+      
+    if path is not None:
+      environ['PATH_INFO'] = path.lstrip('/')
+      
+    if method is not None:
+      environ['REQUEST_METHOD'] = method
+      
+    for k, v in (headers or {}).items():
+      k = k.upper()
+      environ['HTTP_'+k] = v
+
+    if json is not None:
+      environ['CONTENT_TYPE'] = 'application/json'
+      self._set_payload(environ, lib_json.dumps(json).encode('utf8'))
+
+    if url is not None:
+      o = urlparse(url)
+      environ['wsgi.url_scheme'] = o.scheme
+      environ['HTTP_HOST'] = o.netloc
+      environ['PATH_INFO'] = o.path.lstrip('/')
+
+    request = bottle.BaseRequest(environ)
+    for k,v in extras.items():
+      setattr(request, k, v)
+    self.request = request
+    
+  def _set_payload(self, environ, payload):
+    payload = bytes(payload)
+    environ['CONTENT_LENGTH'] = str(len(payload))
+    environ['wsgi.input'] = io.BytesIO(payload)
+
+  def __enter__(self):
+    self.orig = bottle.request
+    bottle.request = self.request
+
+  def __exit__(self,a,b,c):
+      bottle.request = self.orig
+
+
---
+++ tests.py
@@ -0,0 +1,58 @@
+import bottle, unittest
+from boddle import boddle
+
+
+class TestBoddle(unittest.TestCase):
+  
+  def testParams(self):
+    with boddle(params={'name':'derek'}):
+      self.assertEqual(bottle.request.params['name'], 'derek')
+ 
+  def testGetParams(self):
+    with boddle(method='get', params={'name':'derek'}):
+      self.assertEqual(bottle.request.params['name'], 'derek')
+ 
+  def testPostParams(self):
+    with boddle(method='post', params={'name':'derek'}):
+      self.assertEqual(bottle.request.params['name'], 'derek')
+ 
+  def testPath(self):
+    with boddle(path='/derek'):
+      self.assertEqual(bottle.request.path, '/derek')
+      with boddle(path='/anderson'):
+        self.assertEqual(bottle.request.path, '/anderson')
+      self.assertEqual(bottle.request.path, '/derek')
+ 
+  def testMethod(self):
+    with boddle(method='post'):
+      self.assertEqual(bottle.request.method, 'POST')
+ 
+  def testHeaders(self):
+    with boddle(headers={'x_header':'value'}):
+      self.assertEqual(bottle.request.headers['X_HEADER'], 'value')
+ 
+  def testExtraStuff(self):
+    with boddle(extra='woot'):
+      self.assertEqual(bottle.request.extra, 'woot')
+ 
+  def testJSON(self):
+    with boddle(json={'name':'derek'}):
+      self.assertEqual(bottle.request.json['name'], 'derek')
+ 
+  def testURL(self):
+    with boddle(url='https://github.com/keredson/boddle'):
+      self.assertEqual(bottle.request.url, 'https://github.com/keredson/boddle')
+      self.assertEqual(bottle.request.fullpath, '/keredson/boddle')
+      self.assertEqual(bottle.request.path, '/keredson/boddle')
+
+  def testRevert(self):
+    with boddle(params={'name':'derek'}):
+      self.assertEqual(bottle.request.params['name'], 'derek')
+      with boddle(params={'name':'anderson'}):
+        self.assertEqual(bottle.request.params['name'], 'anderson')
+      self.assertEqual(bottle.request.params['name'], 'derek')
+ 
+  
+if __name__=='__main__':
+  unittest.main()
+
